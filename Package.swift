6cacc7792b9268ddcd114914712e817495118a03
Elisabete Bicho <elipasousa@gmail.com>
2021-03-10 12:47:34 +0000

    SPM udpates

--- Package.swift
+++ Package.swift
@@ -6,28 +6,21 @@ import PackageDescription
 let package = Package(
     name: "iCarousel",
     platforms: [
-        .macOS(.v10_12),
-        .iOS(.v10)
+        .iOS(.v9),
+        .macOS(.v10_10),
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "iCarousel",
-            targets: ["iCarousel"])
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["iCarousel"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "iCarousel",
-            dependencies: [],
-            path: ".",
-            sources: ["iCarousel"],
-            publicHeadersPath: "iCarousel"
-        )
+            path: "iCarousel",
+            exclude: ["iCarousel/Info.plist"]),
+        .testTarget(
+            name: "iCarouselTests",
+            dependencies: ["iCarousel"]),
     ]
 )
